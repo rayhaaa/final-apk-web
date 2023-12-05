efac6e7d0a68ebdf3e103bc659fe6e6851a38552
rayhaaa <rayhanayasmin.2021@student.uny.ac.id>
2023-12-05 21:51:19 +0700

    first commit

---
+++ index.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import sklearn
+import joblib
+import numpy as np
+
+def index():
+    st.markdown("dashboard")
+    st.sidebar("dashboard")
+
+def page1():
+    st.markdown("hal analisis sentimen")
+    st.sidebar("dashboard")
---
+++ pages/page1.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.markdown("page 1")
+st.sidebar.markdown("halaman kedua")
+
+st.write("halaman analisis sentimen dari ")
